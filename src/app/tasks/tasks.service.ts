bb1df53e17958d5e70af545f6e804e9a065ac73e
kuldeep-gosiya <kuldeep549@tnraocollege.org>
2023-06-07 19:14:53 +0530

    done

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -14,27 +14,27 @@ export class TasksService {
 
   //retreiving tasks
   getTasks(){
-    return this.http.get('http://test-ng.raoinfo.tech/api/tasks').pipe(map(res=>res));
+    return this.http.get('http://65.2.124.123/api/tasks').pipe(map(res=>res));
   }
 
   addTask(newTask:any){
     var headers=new HttpHeaders();
     //const headers = new HttpHeaders().set('Content-Type', 'application/json')
     headers.append('Content-Type','application/json');
-    return this.http.post('http://test-ng.raoinfo.tech/api/add',newTask,{headers:headers}).pipe(map(res=>res));
+    return this.http.post('http://65.2.124.123/api/add',newTask,{headers:headers}).pipe(map(res=>res));
   }
   deleteTask(id:any){
-    return this.http.delete('http://test-ng.raoinfo.tech/api/delete/'+id).pipe(map(res => res));
+    return this.http.delete('http://65.2.124.123/api/delete/'+id).pipe(map(res => res));
   }
 
   viewTask(id:any){
-    return this.http.get('http://test-ng.raoinfo.tech/api/viewtask/'+id).pipe(map(res => res));
+    return this.http.get('http://65.2.124.123/api/viewtask/'+id).pipe(map(res => res));
   }
 
   updateTask(id:any,newTask:any){
     var headers=new HttpHeaders();
     headers.append('Content-Type','application/json');
-    return this.http.post('http://test-ng.raoinfo.tech/api/update/'+id,newTask,{headers:headers}).pipe(map(res=>res));
+    return this.http.post('http://65.2.124.123/api/update/'+id,newTask,{headers:headers}).pipe(map(res=>res));
   }
 
 }
