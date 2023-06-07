8a01710cca38d923f924f0765869d8951127507c
kuldeep-gosiya <kuldeep549@tnraocollege.org>
2023-06-07 17:28:59 +0530

    ip update in index file

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -14,27 +14,27 @@ export class TasksService {
 
   //retreiving tasks
   getTasks(){
-    return this.http.get('http://65.2.124.123/api/tasks').pipe(map(res=>res));
+    return this.http.get('http://172.26.10.245/api/tasks').pipe(map(res=>res));
   }
 
   addTask(newTask:any){
     var headers=new HttpHeaders();
     //const headers = new HttpHeaders().set('Content-Type', 'application/json')
     headers.append('Content-Type','application/json');
-    return this.http.post('http://65.2.124.123/api/add',newTask,{headers:headers}).pipe(map(res=>res));
+    return this.http.post('http://172.26.10.245/api/add',newTask,{headers:headers}).pipe(map(res=>res));
   }
   deleteTask(id:any){
-    return this.http.delete('http://65.2.124.123/api/delete/'+id).pipe(map(res => res));
+    return this.http.delete('http://172.26.10.245/api/delete/'+id).pipe(map(res => res));
   }
 
   viewTask(id:any){
-    return this.http.get('http://65.2.124.123/api/viewtask/'+id).pipe(map(res => res));
+    return this.http.get('http://172.26.10.245/api/viewtask/'+id).pipe(map(res => res));
   }
 
   updateTask(id:any,newTask:any){
     var headers=new HttpHeaders();
     headers.append('Content-Type','application/json');
-    return this.http.post('http://65.2.124.123/api/update/'+id,newTask,{headers:headers}).pipe(map(res=>res));
+    return this.http.post('http://172.26.10.245/api/update/'+id,newTask,{headers:headers}).pipe(map(res=>res));
   }
 
 }
